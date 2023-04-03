a5fa94748224a5d8f6c551b9cbab61c6aad91991
Luryxyt <36661311+lukaszkusgithub@users.noreply.github.com>
2023-04-03 18:32:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(name='Video-cutting-script', version='1.0', packages=find_packages())
+setup(
+    name='Video-cutting-script',
+    version='1.0',
+    description='Video editing application',
+    url='https://github.com/PiotrZb/Video-cutting-script',
+    license='GNU General Public License (GPL)',
+    author='Piotr Zborowski, Łukasz Kus',
+    platforms='Windows / Unix',
+    long_description='',
+    packages=find_packages())
