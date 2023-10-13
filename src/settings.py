661327b0b9ae3aa44cc53677c029a229f891a1a8
Piotr Zborowski <piotrz2000zetor@gmail.com>
2023-10-13 13:09:15 +0200

    New objects added to settings

--- src/settings.py
+++ src/settings.py
@@ -2,6 +2,7 @@ from src.pathmanager import PathManager
 
 VIDEO_FILE_EXTENSIONS = ('.mp4', '.m4v', '.m4p')
 IMAGE_FILE_EXTENSIONS = ('.jpg', 'jpeg', '.png')
-CLASS_ID = {'rock':0, 'tree':1, 'car parts':2, 'car wheel':3, 'snow':4, 'bird':5}
+CLASS_ID = {'rock': 0, 'tree': 1, 'car parts': 2, 'car wheel': 3, 'snow': 4, 'bird': 5, 'plank': 6, 'animal': 7,
+            'undefined object': 8}
 
 path_manager = PathManager()
